75f1e7c6ff0c3f9b2b4020cc6c54505494bde50b
Stanislav Maruniak <st.maruniak@gmail.com>
2017-03-03 22:07:46 +0200

    valid url

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,4 +3,5 @@ class Url < ApplicationRecord
 
   validates :long, presence: true, uniqueness: true
   validates :custom, :short, uniqueness: true, allow_blank: true
+  validates :long, format: { with: URI::regexp(%w(http https)), message: 'Valid URL required'}
 end
