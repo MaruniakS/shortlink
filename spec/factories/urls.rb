b7cc7feaa343fa8b76c7e7579fe7ff4c56ceb5f5
Maruniak <stanislav.maruniak@kindgeek.com>
2017-03-07 13:10:28 +0200

    testss

--- spec/factories/urls.rb
+++ spec/factories/urls.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :url do
-    long { Faker::Lorem.word }
+    long { Faker::Internet.url }
     created_by { Faker::Number.number(10) }
   end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -8,21 +8,32 @@ RSpec.describe Url, type: :model do
 
   let(:link) { Url.create!(long: 'http://example.com') }
 
-  it 'creates not blank short url' do
-    expect(link.short.blank?).to eq false
+  context 'url validation' do
+    it 'creates not blank short url' do
+      expect(link.short.blank?).to eq false
+    end
+
+    it 'should not save duplicated long url' do
+      duplicated_link = Url.new(long: link.long)
+      expect(duplicated_link).to be_invalid
+    end
+
+    it 'should accept only correct format of url' do
+      test_urls = Url.create([{ long: 'example.com' }, { long: 'htp://error.url' }, { long: 'http//error.url' }])
+      test_urls.each do |url|
+        expect(url.errors).to_not be_empty
+      end
+    end
+
+    it 'should not save duplicated custom url' do
+      second_link = Url.create(long: 'http://us.com', custom: link.short)
+      expect(second_link.errors).to_not be_empty
+    end
   end
 
-  it 'should not save duplicated long url' do
-    duplicated_link = Url.new(long: link.long)
-    expect(duplicated_link).to be_invalid
-  end
-
-  it 'should not save duplicated custom url' do
-    second_link = Url.create(long: 'http://us.com', custom: link.short)
-    expect(second_link.errors).to_not be_empty
-  end
-
-  it 'should return correct id' do
-    expect(link.get_id).to eq link.id
+  context 'url methods' do
+    it 'should return correct id' do
+      expect(Url.get_id(link.short)).to eq link.id
+    end
   end
 end
