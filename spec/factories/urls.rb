3a9c3ca7d6c732583909ad192ee4a071842406e6
Stanislav Maruniak <st.maruniak@gmail.com>
2017-03-09 22:17:57 +0200

    tests

--- spec/factories/urls.rb
+++ spec/factories/urls.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :url do
     long { Faker::Internet.url }
-    created_by { Faker::Number.number(10) }
+    created_at { Faker::Number.number(10) }
   end
 end
--- spec/requests/urls_spec.rb
+++ spec/requests/urls_spec.rb
@@ -3,5 +3,19 @@ require 'rails_helper'
 RSpec.describe 'urls API', type: :request do
   # initialize test data
   let!(:urls) { create_list(:url, 10) }
-  let(:url_id) { urls.first.id }
+  let(:url) { urls.first }
+
+  it 'creates an url' do
+    get '/'
+    expect(response).to render_template(:index)
+    post '/urls', url: { long: 'http://url.long' } , format: :js
+    expect(response.content_type).to eq Mime[:js]
+  end
+
+=begin
+  it "does not render a different template" do
+    get "/widgets/new"
+    expect(response).to_not render_template(:show)
+  end
+=end
 end
